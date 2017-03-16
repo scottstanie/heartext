2c9f669d1b7ccab9971cf98cb2932e2bbc268c77
scott <scott@cogolabs.com>
2017-03-15 23:11:32 -0400

    add region name

--- polly/txt_to_mp3.py
+++ polly/txt_to_mp3.py
@@ -12,7 +12,8 @@ from heartext import settings
 
 session = Session(
     aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
-    aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY
+    aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
+    region_name='us-east-1',
 )
 polly = session.client("polly")
 

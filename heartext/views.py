56742d7e25c7ac16f3d8422897c90b94c9279615
scott <scott@cogolabs.com>
2017-03-18 19:44:00 -0400

    add check for pdf or html to url request

--- heartext/views.py
+++ heartext/views.py
@@ -1,6 +1,7 @@
 import json
 import requests
 import subprocess
+
 from django.shortcuts import render
 from django.http import JsonResponse  # HttpResponse, HttpResponseRedirect
 from django.views.decorators.csrf import ensure_csrf_cookie
@@ -14,9 +15,8 @@ def index(request):
 def parse(request):
     """Gets the URL submitted and returns the text from it
     """
-    def _download(url):
-        r = requests.get(url)
-        with open('./tmp.html', 'wb') as f:
+    def _download(url, filename):
+        with open(filename, 'wb') as f:
             f.write(r.content)
 
     def _read():
@@ -26,15 +26,31 @@ def parse(request):
     body = json.loads(request.body)
     input_url = body.get('url')
     print 'Extracting: ', input_url
-    _download(input_url)
 
+    r = requests.get(input_url)
+    ext = get_response_type(r)
+    filename = './tmp.{}'.format(ext)
+    _download(input_url, filename)
     subprocess.check_call(['java',
                            '-jar',
                            'boilerpipe-core/dist/boilerpipe-1.2-dev.jar',
-                           './tmp.html',
+                           filename,
                            'out.txt'])
 
     return JsonResponse({"OK": True, "text": _read()})
+
+
+def get_response_type(response):
+    content_type = response.headers.get('content-type')
+
+    if 'application/pdf' in content_type:
+        ext = '.pdf'
+    elif 'text/html' in content_type:
+        ext = '.html'
+    else:
+        ext = ''
+        print 'Unknown type: {}'.format(content_type)
+    return ext
 
 
 def pdf_to_text(pdf_document):

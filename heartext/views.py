182484bb85775edfa620638e516cc9620fe4dfe3
scott <scott@cogolabs.com>
2017-03-18 22:19:46 -0400

    pdf file upload working

--- heartext/views.py
+++ heartext/views.py
@@ -1,6 +1,8 @@
 import json
 import requests
 import subprocess
+import textract
+import mimetypes
 
 from django.shortcuts import render
 from django.http import JsonResponse  # HttpResponse, HttpResponseRedirect
@@ -13,9 +15,33 @@ def index(request):
 
 
 def upload(request):
-    print request.FILES
-    print request.POST
-    return JsonResponse({"OK": True})
+    def _handle_uploaded_file(f, filename='tmp.pdf'):
+        """Write the submitted file to a temporary pdf"""
+        with open(filename, 'wb+') as destination:
+            for chunk in f.chunks():
+                destination.write(chunk)
+    f = request.FILES.get('file')
+    if not f:
+        return JsonResponse({"OK": False})
+
+    if f.content_type != 'application/pdf':
+        print "Can only handle PDF files right now"
+        return JsonResponse({"OK": False})
+
+    filename = 'tmp.pdf'
+    _handle_uploaded_file(f, filename)
+    out_text = extract_pdf(filename)
+    # Save the output text file for now to analyze
+    with open(filename.replace('.pdf', '.txt'), 'w') as f:
+        f.write(out_text)
+    return JsonResponse({"OK": True, "text": out_text})
+
+
+def extract_pdf(filename):
+    """Use the textract library to get plain text from the pdf"""
+    if mimetypes.guess_type(filename)[0] != 'application/pdf':
+        return ''
+    return textract.process(filename)
 
 
 def parse(request):
@@ -29,12 +55,24 @@ def parse(request):
         with open('./out.txt', 'rb') as f:
             return f.read()
 
+    def _get_response_type(response):
+        content_type = response.headers.get('content-type')
+
+        if 'application/pdf' in content_type:
+            ext = '.pdf'
+        elif 'text/html' in content_type:
+            ext = '.html'
+        else:
+            ext = ''
+            print 'Unknown type: {}'.format(content_type)
+        return ext
+
     body = json.loads(request.body)
     input_url = body.get('url')
     print 'Extracting: ', input_url
 
     r = requests.get(input_url)
-    ext = get_response_type(r)
+    ext = _get_response_type(r)
     filename = './tmp.{}'.format(ext)
     _download(input_url, filename)
     subprocess.check_call(['java',
@@ -45,18 +83,6 @@ def parse(request):
 
     return JsonResponse({"OK": True, "text": _read()})
 
-
-def get_response_type(response):
-    content_type = response.headers.get('content-type')
-
-    if 'application/pdf' in content_type:
-        ext = '.pdf'
-    elif 'text/html' in content_type:
-        ext = '.html'
-    else:
-        ext = ''
-        print 'Unknown type: {}'.format(content_type)
-    return ext
 
 
 def pdf_to_text(pdf_document):
